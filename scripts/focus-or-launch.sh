6799533a7ea509b67decdcbd0a29cc5da4b0fe3a
Kostas <klampros@priogen.nl>
2026-05-12 14:18:57 +0200

    imporve focus script

--- scripts/focus-or-launch.sh
+++ scripts/focus-or-launch.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-# ~/.local/bin/focus-or-launch.sh
-# Usage: focus-or-launch.sh <window-name> <launch-command>
-
 WINDOW_NAME="$1"
-LAUNCH_CMD="$2"
+shift
+LAUNCH_CMD=("$@")
 
-if kdotool search --name $WINDOW_NAME >/dev/null 2>&1; then
-  kdotool search --name $WINDOW_NAME windowactivate
+name_result=$(kdotool search --name "$WINDOW_NAME" | head -1 2>/dev/null)
+echo "$name_result"
+
+if [[ -n $name_result ]]; then
+  # echo "window found"
+  kdotool windowactivate "$name_result"
 else
-  $LAUNCH_CMD &
+  # echo "no window - launching new"
+  "${LAUNCH_CMD[@]}" &
 fi
